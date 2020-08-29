a3dd251e36e6022cf453c5af5c907b201c6f1d86
Dennis <Dezell2002@gmail.com>
2020-08-29 14:13:44 -0500

    switch to config based configuration for identity server:w

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,47 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("Mplus", "Mythic Plus Signup API")
+            };
+        }
+
+        /// <summary>
+        ///     Using In-memory clients for the time being
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "MPlus Client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedScopes = { "Mplus" }
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email()
+            };
+        }
+    }
+}
